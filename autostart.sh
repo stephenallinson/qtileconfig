b767460c857bc7e0217bb8bf2400a48bd81f210e
stephen.allinson <stephen.allinson@gmail.com>
2024-05-13 09:01:06 -0500

    Update swww in autostart to match current spec

--- autostart.sh
+++ autostart.sh
@@ -6,7 +6,7 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 kanshi &
 playerctld daemon &
 swaync &
-swww init &
+swww-daemon &
 wlsunset -l 49.8 -L 97.1 &
 wl-mpris-idle-inhibit &
 swayidle -w timeout 300 'swaylock -f -c 000000' timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' before-sleep 'swaylock -f -c 000000' &
