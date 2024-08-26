5ac9f9d5781cc3c75b6a5e3d3117e0969c851c8e
stephen.allinson <stephen.allinson@gmail.com>
2024-08-26 14:20:30 -0500

    re-enable swayidle after qtile fix

--- autostart.sh
+++ autostart.sh
@@ -8,5 +8,5 @@ swaync &
 swww-daemon &
 wlsunset -l 49.8 -L 97.1 &
 wl-mpris-idle-inhibit &
-# swayidle -w timeout 300 'swaylock -f -c 000000' timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' before-sleep 'swaylock -f -c 000000' &
+swayidle -w timeout 300 'swaylock -f -c 000000' timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' before-sleep 'swaylock -f -c 000000' &
 easyeffects --gapplication-service &
